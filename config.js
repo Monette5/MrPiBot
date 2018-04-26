f59ab1d7116ee72039cc6910e47c15b628aa1154
monette5 <darcy4@hotmail.com>
2018-04-26 22:37:13 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'R3xVo9DM9eaoQ66WhrG7DZtJl',  
+  consumer_secret: 'Gq1fqlBKpNrsv15oJdWQUXzxImlQFLiUF79ODojL4nK9NZiZ8Q',
+  access_token: '262321984-50b8jIzJjr3UiZV2Vrfxjvzu2IaLQ8m5HPlt5kQr',  
+  access_token_secret: 'nBJshJCWKbYF6PcUyFLIffwImsSDrj50chxUAPA8kCLvC'
+}
