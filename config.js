9873d2361e74eeb3a25c681d91f30c7253ba5cdf
Monette <darcy4@hotmail.com>
2018-04-26 22:40:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'R3xVo9DM9eaoQ66WhrG7DZtJl',  
-  consumer_secret: 'Gq1fqlBKpNrsv15oJdWQUXzxImlQFLiUF79ODojL4nK9NZiZ8Q',
-  access_token: '262321984-50b8jIzJjr3UiZV2Vrfxjvzu2IaLQ8m5HPlt5kQr',  
-  access_token_secret: 'nBJshJCWKbYF6PcUyFLIffwImsSDrj50chxUAPA8kCLvC'
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
 }
